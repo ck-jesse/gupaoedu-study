e9ead692e3e38b957632a1d4db278126a8aee8c6
chenck <chenchuangkunWJ69580@touna.cn>
2019-04-03 14:27:20 +0800

    添加几个模块包

---
+++ study-mybatis/src/main/java/com/coy/gupaoedu/study/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.coy.gupaoedu.study.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
