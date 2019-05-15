485843d89912674b71d63ad8dacaafefebfafb45
chenck <chenchuangkunWJ69580@touna.cn>
2019-05-15 16:24:06 +0800

    模块迁移，统一模块的命名规则 将gupaoedu-study-pattern模块迁移到study-pattern模块 将gupaoedu-study-spring模块迁移到study-spring模块

---
+++ study-pattern/src/main/java/com/coy/gupaoedu/study/pattern/factory/App.java
@@ -0,0 +1,13 @@
+package com.coy.gupaoedu.study.pattern.factory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
