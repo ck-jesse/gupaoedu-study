3b030f49da4a7e3e88c5bac0d8d39d49ba5d2a01
chenck <244926053@qq.com>
2019-06-02 22:23:09 +0800

    添加rpc和serialization模块

---
+++ study-rpc/src/main/java/com/coy/gupaoedu/study/rpc/App.java
@@ -0,0 +1,34 @@
+package com.coy.gupaoedu.study.rpc;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    /*
+
+    rpc相关的知识
+1、协议
+	七层网络协议
+	http协议
+	rpc协议：
+	tcp/ip
+2、http和https
+
+3、数据序列化
+	java原生序列化
+	hessian
+	fastjson
+	jackson
+	gson
+	xml
+	protobuf
+	不同的序列化后的数据大小不同，越小传输性能越高
+	实现样例，体现对象转字节流后的大小，以及数据格式也会影响大小
+
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
