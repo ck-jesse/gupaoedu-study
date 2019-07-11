dd063aff3ceb716fa13cda1870d99e55a18fab39
chenck <244926053@qq.com>
2019-07-11 22:04:20 +0800

    梳理leader选举实现的原理

--- study-zookeeper/src/main/java/com/coy/gupaoedu/study/zookeeper/ZookeeperClient.java
+++ study-zookeeper/src/main/java/com/coy/gupaoedu/study/zookeeper/ZookeeperClient.java
@@ -132,16 +132,21 @@ public class ZookeeperClient {
     /**
      * 分布式锁
      * <p>
-     * 本质：
+     * 原理：
      * 1、创建一个临时有序节点（此时未持有锁）
-     * org.apache.curator.framework.recipes.locks.LockInternalsDriver#createsTheLock
-     * 2、自旋判断是否能够持有锁，
-     * org.apache.curator.framework.recipes.locks.LockInternals#internalLockLoop
+     * 方法：org.apache.curator.framework.recipes.locks.LockInternalsDriver#createsTheLock
+     * 2、自旋（死循环）判断当前节点是否能够持有锁
+     * 方法：org.apache.curator.framework.recipes.locks.LockInternals#internalLockLoop
      * 3、获取到所有子节点列表，并且从小到大根据节点名称后10位数字进行排序；
      * 若能持有锁，则返回true；
      * 若不能持有锁，则获取到上一个临时节点的path，并添加一个watcher监听（也就是当前线程会监听自己节点的上一个节点的变化，而不是监听父节点下所有节点的变动）；
      * 然后当前线程进入wait，等待被唤醒；
      * 当监听的节点发生了变动时，那么就将线程从等待状态唤醒，重新开始下一轮的锁的争抢。
+     * <p>
+     * 总结：
+     * 分布式锁的本质就是通过创建一个临时有序节点，和watcher监听上一个节点的变动来实现的。
+     *
+     * 利用临时有序节点+watcher监听机制可以做很多事情，如分布式锁、Leader选举、分布式队列（FIFO）
      */
     public void distributedLock(String path, long waitTime, TimeUnit unit) {
         // Curator的几种锁方案
@@ -181,6 +186,16 @@ public class ZookeeperClient {
 
     /**
      * leader选举
+     * <p>
+     * 原理：
+     * 1、创建一个线程池，然后将在执行 LeaderSelector.start()方法时,王线程池中提交一个workloop任务
+     * 2、执行任务对应方法 LeaderSelector.doWork()，其中使用了分布式锁InterProcessMutex.acquire()来无限等待获得锁
+     * 3、若当前节点获得锁，则执行 LeaderSelectorListener.takeLeadership()方法，也就是自定义的Listener中的方法
+     * 当该方法持有执行权限时，则表示当前节点被选举为leader了，退出该方法时，则表示放弃执行权限，放弃领导权
+     * 4、若当前节点未获得锁，则无限等待获得锁（实质就是利用了分布式锁的特性来控制）
+     * <p>
+     * 总结：
+     * leader选举的本质就是利用的分布式锁的特性来实现的主从选举。watcher监听上一个临时有序节点，然后无限等待，直到获得锁，获得锁则表示被选举为了leader。
      */
     public void leaderElection(String path) {
         LeaderSelectorListener listener = new LeaderSelectorListenerAdapter() {
@@ -191,8 +206,8 @@ public class ZookeeperClient {
                 // 当你是领导者，做你需要做的任何领导工作时，这个回调都会被调用，只有当你想放弃领导时才退出这个方法。
                 System.out.println("我是Leader啦，听我号令，开干咯！我要一直保持不return该方法，一但return那么我就不是leader啦");
                 try {
-                    System.out.println("我将休眠20s，这段时间我是leader");
-                    Thread.sleep(20000);
+                    System.out.println("我将休眠10s，这段时间我是leader");
+                    Thread.sleep(10000);
                 } finally {
                     System.out.println("relinquishing leadership 放弃领导权");
                 }
