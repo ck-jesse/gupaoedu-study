6f3b1d6a2cd372dfee9686d77ec3ee5087c6c446
chenck <chenchuangkunWJ69580@touna.cn>
2019-07-31 11:12:59 +0800

    阅读dubbo源码的测试

--- study-pattern/src/main/java/com/coy/gupaoedu/study/pattern/observer/listener/jdk/MouseListenerTest.java
+++ study-pattern/src/main/java/com/coy/gupaoedu/study/pattern/observer/listener/jdk/MouseListenerTest.java
@@ -5,6 +5,14 @@ package com.coy.gupaoedu.study.pattern.observer.listener.jdk;
  * 包含三个角色：事件-事件源-监听器
  * 观察者模式的一种，源-收听者(Listener)模式
  *
+ * 注：spring中的事件机制就是基于JDK的事件-监听机制来实现的
+ * 如：ApplicationListener 实现EventListener 和 ApplicationEvent 继承EventObject
+ * 1、spring加载时会获取到所有实现了ApplicationListener的监听器，并维护到集合Set<ApplicationListener>中
+ * 2、事件触发点的逻辑中，通过ApplicationEventPublisher#publishEvent(ApplicationEvent)来发布事件
+ * 3、publishEvent中会根据事件来源source和事件类型type，从集合中匹配对应的ApplicationListener集合
+ * 4、然后循环匹配的监听器集合，调用具体的事件处理方法ApplicationListener.onApplicationEvent(ContextRefreshedEvent等Event)
+ *
+ *
  * @author chenck
  * @date 2019/3/19 14:19
  */
