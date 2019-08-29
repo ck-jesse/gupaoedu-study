84874fe78b296237ca384f14db6a904f199eb08c
chenck <chenchuangkunWJ69580@touna.cn>
2019-08-29 17:58:47 +0800

    idea快捷键的测试（Alt+Enter只能提示编辑json格式字符串等）

--- study-juc/src/main/java/com/coy/gupaoedu/study/juc/collection/DelayQueueTest.java
+++ study-juc/src/main/java/com/coy/gupaoedu/study/juc/collection/DelayQueueTest.java
@@ -1,5 +1,9 @@
 package com.coy.gupaoedu.study.juc.collection;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * DelayQueue 延时队列
  * 可以使放入队列的元素在指定的延时后才被消费者取出，元素需要实现Delayed接口。
@@ -7,5 +11,22 @@ package com.coy.gupaoedu.study.juc.collection;
  * @author chenck
  * @date 2019/8/26 12:53
  */
-public class DelayQueueTest {
+public class DelayQueueTest implements Serializable {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        // 快捷键 list.for => for (String s : list) {}
+        for (String s12 : list) {
+
+        }
+
+        String json = "{\n" +
+                "  \"name\":\"zhangsanfeng\",\n" +
+                "  \"age\": 182345667,\n" +
+                "  \"desc\": \"保持年轻\"\n" +
+                "}";
+        String java = "public void test(){\n" +
+                "    sytstem.out.println(\"test\");\n" +
+                "}";
+    }
 }
