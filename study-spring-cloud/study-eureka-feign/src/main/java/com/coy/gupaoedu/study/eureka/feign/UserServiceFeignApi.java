e4b540739c571390b64ed039f5f255c36bbb98a0
chenck <chuangkun.chen@drgou.com>
2019-12-06 17:16:22 +0800

    基于feign的单个方法的超时时间设置

--- study-spring-cloud/study-eureka-feign/src/main/java/com/coy/gupaoedu/study/eureka/feign/UserServiceFeignApi.java
+++ study-spring-cloud/study-eureka-feign/src/main/java/com/coy/gupaoedu/study/eureka/feign/UserServiceFeignApi.java
@@ -1,5 +1,6 @@
 package com.coy.gupaoedu.study.eureka.feign;
 
+import feign.Request;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,4 +19,14 @@ public interface UserServiceFeignApi {
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     String home(@RequestParam(value = "name") String name);
+
+    /**
+     * 增加一个方法入参 Request.Options options，用于设置单个方法的连接超时时间、读取超时时间
+     * 注：通过分析源码，得知可通过该方式来设置单个方法的超时时间：
+     * feign.SynchronousMethodHandler#invoke(Object[] argv) => feign代理对象的执行入口
+     * feign.SynchronousMethodHandler#findOptions(argv) => 关键点在这里
+     * findOptions(argv) 方法会判断方法入参数组对象中是否包含了Request.Options类型的对象，如果有，则取出，如果没有，则取默认的Request.Options
+     */
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    String home(@RequestParam(value = "name") String name, Request.Options options);
 }
