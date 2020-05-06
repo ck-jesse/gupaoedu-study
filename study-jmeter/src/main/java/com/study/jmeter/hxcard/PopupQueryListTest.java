030e91e7ff82a01233d76f9272012184229e41f2
chenck <chuangkun.chen@drgou.com>
2020-05-06 17:14:43 +0800

    权益卡服务jmeter压测脚本

--- study-jmeter/src/main/java/com/study/jmeter/hxcard/PopupQueryListTest.java
+++ study-jmeter/src/main/java/com/study/jmeter/hxcard/PopupQueryListTest.java
@@ -1,6 +1,6 @@
 package com.study.jmeter.hxcard;
 
-import com.hx.card.service.proto.CardEquityProto;
+import com.hx.card.service.proto.PopupConfigProto;
 import com.study.jmeter.common.RestTemplateInstance;
 import com.study.jmeter.common.UrlUtil;
 import org.apache.jmeter.config.Arguments;
@@ -10,20 +10,17 @@ import org.apache.jmeter.samplers.SampleResult;
 import org.springframework.util.StringUtils;
 
 /**
- * 花享卡权益
+ * popup 列表
  *
  * @author chenck
  * @date 2020/4/30 14:24
  */
-public class CardEquityQueryListTest extends AbstractJavaSamplerClient {
+public class PopupQueryListTest extends AbstractJavaSamplerClient {
 
     private String ip;
     private Integer port;
-    private Integer supplierId;// 供应商id，关联供应商表主键
-    private Integer catalogId;// 分类id，关联分类表主键
-    private String name;// 权益名
-    private Integer isRecommended;// 是否推荐, 1:不推荐，2:推荐
-    private Integer status;// 状态，1:上架，2:下架
+    private String title;// 标题
+    private Integer status;// 状态:1上架,2下架
     private Integer useCache;// 是否使用缓存，1表示不使用缓存，0或其他值则表示使用缓存
     private Integer pageNum;
     private Integer pageSize;
@@ -33,10 +30,7 @@ public class CardEquityQueryListTest extends AbstractJavaSamplerClient {
         Arguments params = new Arguments();
         params.addArgument("ip", "172.18.219.222");
         params.addArgument("port", "80");
-        params.addArgument("supplierId", "0");
-        params.addArgument("catalogId", "0");
-        params.addArgument("name", "");
-        params.addArgument("isRecommended", "2");
+        params.addArgument("title", "");
         params.addArgument("status", "1");
         params.addArgument("useCache", "0");
         params.addArgument("pageNum", "1");
@@ -48,10 +42,7 @@ public class CardEquityQueryListTest extends AbstractJavaSamplerClient {
     public void setupTest(JavaSamplerContext arg0) {
         ip = arg0.getParameter("ip");
         port = arg0.getIntParameter("port");
-        supplierId = arg0.getIntParameter("supplierId");
-        catalogId = arg0.getIntParameter("catalogId");
-        name = arg0.getParameter("name");
-        isRecommended = arg0.getIntParameter("isRecommended");
+        title = arg0.getParameter("title");
         status = arg0.getIntParameter("status");
         useCache = arg0.getIntParameter("useCache");
         pageNum = arg0.getIntParameter("pageNum");
@@ -63,20 +54,11 @@ public class CardEquityQueryListTest extends AbstractJavaSamplerClient {
         SampleResult result = new SampleResult();
         // 开始计时
         result.sampleStart();
-        String url = UrlUtil.buildURL(ip, port, "/hx-card-service/cardEquity/queryCardEquityList");
+        String url = UrlUtil.buildURL(ip, port, "/hx-card-service/popupConfig/queryPopupConfigList");
 
-        CardEquityProto.CardEquityQueryListRequest.Builder builder = CardEquityProto.CardEquityQueryListRequest.newBuilder();
-        if (null != supplierId) {
-            builder.setSupplierId(supplierId);
-        }
-        if (null != catalogId) {
-            builder.setCatalogId(catalogId);
-        }
-        if (!StringUtils.isEmpty(name)) {
-            builder.setName(name);
-        }
-        if (null != isRecommended) {
-            builder.setIsRecommended(isRecommended);
+        PopupConfigProto.PopupConfigQueryListRequest.Builder builder = PopupConfigProto.PopupConfigQueryListRequest.newBuilder();
+        if (!StringUtils.isEmpty(title)) {
+            builder.setTitle(title);
         }
         if (null != status) {
             builder.setStatus(status);
@@ -86,8 +68,8 @@ public class CardEquityQueryListTest extends AbstractJavaSamplerClient {
         }
         builder.setPageNum(pageNum);
         builder.setPageSize(pageSize);
-        CardEquityProto.CardEquityQueryListResponse response = RestTemplateInstance.protoRestTemplate.postForObject(url, builder.build(),
-                CardEquityProto.CardEquityQueryListResponse.class);
+        PopupConfigProto.PopupConfigQueryListResponse response = RestTemplateInstance.protoRestTemplate.postForObject(url, builder.build(),
+                PopupConfigProto.PopupConfigQueryListResponse.class);
         String json = RestTemplateInstance.jsonFormat.printToString(response);
 
         // 计时结束
