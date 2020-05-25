57fb06d53470fcf0f235e840aa4bb0df935cb370
chenck <chuangkun.chen@drgou.com>
2020-05-25 16:22:45 +0800

    二级缓存组件的简介

--- study-spring-demo/src/main/java/com/coy/gupaoedu/study/spring/cache/common/RedisConfig.java
+++ study-spring-demo/src/main/java/com/coy/gupaoedu/study/spring/cache/common/RedisConfig.java
@@ -3,6 +3,7 @@ package com.coy.gupaoedu.study.spring.cache.common;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -21,6 +22,7 @@ public class RedisConfig {
      * 替换RedisTemplate的默认jdk序列化为json序列化
      */
     @Bean
+    @ConditionalOnMissingBean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate redisTemplate = new RedisTemplate();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
