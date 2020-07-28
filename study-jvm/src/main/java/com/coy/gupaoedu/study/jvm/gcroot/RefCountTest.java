5a6373f392f24efeb7dcee8bf8407ad39c330505
chenck <chuangkun.chen@drgou.com>
2020-07-28 10:15:10 +0800

    mysql事务和锁分析

---
+++ study-jvm/src/main/java/com/coy/gupaoedu/study/jvm/gcroot/RefCountTest.java
@@ -0,0 +1,18 @@
+package com.coy.gupaoedu.study.jvm.gcroot;
+
+/**
+ * 引用计数
+ * 标记-清除用来解决引用计数机制产生的循环引用，进而导致内存泄漏的问题 。
+ * <p>
+ * 垃圾回收机制的一种。
+ * Java 中采用的是可达性分析GCRoot来进行垃圾回收的。
+ *
+ * @author chenck
+ * @date 2020/7/28 9:32
+ */
+public class RefCountTest {
+
+    public static void main(String[] args) {
+
+    }
+}
