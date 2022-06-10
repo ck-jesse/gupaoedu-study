de4a06928883a6ffbcfd114086692e7c31ab92c0
chenck <cck1112@163.com>
2022-06-10 23:13:26 +0800

    新增关于forkjoinpool和completablefuture的demo

--- study-juc/src/main/java/com/coy/gupaoedu/study/juc/forkjoin/ForkJoinPoolTest.java
+++ study-juc/src/main/java/com/coy/gupaoedu/study/juc/forkjoin/ForkJoinPoolTest.java
@@ -1,6 +1,5 @@
 package com.coy.gupaoedu.study.juc.forkjoin;
 
-import org.junit.Test;
 
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
@@ -8,14 +7,21 @@ import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 /**
+ * 文档：http://ifeve.com/tag/forkjoinpool/
+ * <p>
  * ForkJoin使用分而治之的思想，把一个大任务拆分成一个个小任务，然后再聚合，得到最终结果。这有点像Hadoop中的MapReduce。还支持工作窃取。
  * Fork就是拆分，Join就是合并。当然，在使用ForkJoinPool构建异步模型时，对于不需要合并(Join)的任务也非常适用。
  * 通过工作窃取（work-stealing），可以充分利用CPU多核进行复杂计算，避免线程进入阻塞或闲置状态。这对java的并发处理能力是很大的提升，同时也避免了大范围使用锁带来的问题。
+ * ForkJoinPool 的每个工作线程都维护着一个工作队列（WorkQueue），这是一个双端队列（Deque），里面存放的对象是任务（ForkJoinTask）。
+ * 每个工作线程在运行中产生新的任务（通常是因为调用了 fork()）时，会放入工作队列的队尾，并且工作线程在处理自己的工作队列时，使用的是 LIFO 方式，也就是说每次从队尾取出任务来执行。
+ * 使用后进先出 —— LIFO用来处理每个工作线程的自己任务，但是使用先进先出 —— FIFO规则用于获取别的任务，这是一种被广泛使用的进行递归Fork/Join设计的一种调优手段。
+ * 让窃取任务的线程从队列拥有者相反的方向进行操作会减少线程竞争。同样体现了递归分治算法的大任务优先策略。
  * <p>
  * ForkJoinPool 中的任务分为两种：
  * 1、本地提交的任务：submission task，如：execute/submit提交的任务
  * 2、fork出来的子任务：work task
  * execute() 方法提交的Runnable类型任务，将会转换为ForkJoinPool类型任务，但由于Runnable类型任务是不可切分的，所以这类型任务无法获得任务拆分(Fork)这方面的收益，不过任然可以获得任务窃取带来的好处和性能提升
+ * <p>
  * <p>
  * Java 7开始引入了一种新的Fork/Join线程池，它可以执行一种特殊的任务：把一个大任务拆成多个小任务并行执行。
  * 我们举个例子：如果要计算一个超大数组的和，最简单的做法是用一个循环在一个线程内完成： *
