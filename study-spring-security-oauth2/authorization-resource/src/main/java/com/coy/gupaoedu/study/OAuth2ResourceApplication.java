aa2b0d609db0a3234f949eb21ead52fd664fbb22
WG-chenchuangkun <wg123456>
2023-04-12 16:45:39 +0800

    Spring Authorization resource模块demo跑通

--- study-spring-security-oauth2/authorization-resource/src/main/java/com/coy/gupaoedu/study/OAuth2ResourceApplication.java
+++ study-spring-security-oauth2/authorization-resource/src/main/java/com/coy/gupaoedu/study/OAuth2ResourceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OAuth2ResourceApplication {
     /**
      * http://localhost:8001/res
-     * 暂未走通
      */
     public static void main(String[] args) {
         SpringApplication.run(OAuth2ResourceApplication.class, args);
